9b281302eb925a442e5de23af6875949fc05d18b
inubayuaji <inubayuaji@gmail.com>
2019-10-26 18:13:46 +0700

    Add home route

---
+++ app/Http/Controllers/HomeCtrl.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeCtrl extends Controller
+{
+    // halaman utama
+    function index()
+    {
+        return null;
+    }
+}
